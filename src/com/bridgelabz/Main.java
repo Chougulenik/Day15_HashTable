a3879fd27edfa7bf184120e7cf0587ac7fb49d13
Chougulenik <nikitachougule47@gmail.com>
2022-03-27 14:38:08 -0400

    added

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,9 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		MyHashFunction func = new MyHashFunction("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations",4); 
-		//func.deleteData("Paranoids");
+		func.deleteData("Paranoids");
 		func.showTable();
 
 	}
-
 }
